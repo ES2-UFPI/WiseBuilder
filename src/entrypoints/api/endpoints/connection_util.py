1e3319c5bcd027dd6f7dfb0dcfc949cdf48e2f6c
Wesley Vitor <wesleyvitor37417@gmail.com>
2023-02-02 02:00:27 -0300

    atualização dos requirements.txt e da função de criação de engine #4

--- src/entrypoints/api/endpoints/connection_util.py
+++ src/entrypoints/api/endpoints/connection_util.py
@@ -5,7 +5,6 @@ from framework.application.handler import MessageBus
 from SearchEngine.application.handlers import COMMAND_HANDLER_MAPPER
 
 def _message_bus(engine):
-    
     uow = MockUnitOfWork(create_session(engine))
     COMMAND_HANDLER_MAPPER_CALLABLE = {}
     for c, h in COMMAND_HANDLER_MAPPER.items():
@@ -13,6 +12,12 @@ def _message_bus(engine):
     
     return MessageBus(uow, {}, COMMAND_HANDLER_MAPPER_CALLABLE)
 
-engine = create_new_engine()
+_db_username = 'root'
+_db_password = '12345678'
+_db_address  = '127.0.0.1'
+_db_port     = '3306'
+_db_name     = 'wisebuilder'
+
+engine = create_new_engine(_db_username, _db_password, _db_address, _db_port, _db_name)
 create_db(engine)
 message_bus = _message_bus(engine)
--- src/framework/infrastructure/db_management/db_connection.py
+++ src/framework/infrastructure/db_management/db_connection.py
@@ -2,13 +2,7 @@ from sqlalchemy.engine.base import Engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_username = 'root'
-db_password = '12345678'
-db_address  = '127.0.0.1'
-db_port     = '3306'
-db_name     = 'wisebuilder'
-
-def create_new_engine() -> Engine:
+def create_new_engine(db_username, db_password, db_address, db_port, db_name) -> Engine:
     engine : Engine = create_engine(f'mariadb+mariadbconnector://{db_username}:{db_password}@{db_address}:{db_port}/{db_name}')
     return engine
 
