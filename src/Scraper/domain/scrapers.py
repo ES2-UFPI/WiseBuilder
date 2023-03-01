6acf5d9a23d7de194b276dc4bce3b340e1c86d59
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-03-01 19:38:55 -0300

    tratamento de erros e excecao #29

--- src/Scraper/domain/scrapers.py
+++ src/Scraper/domain/scrapers.py
@@ -1,27 +1,44 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from value_object import AbstractScraper
 from framework.domain.value_object import URL, Money
+from framework.domain.components import EComponentType
 from bs4 import BeautifulSoup
 import requests as rq
 
 
 @dataclass
 class KabumScraper(AbstractScraper):
-    raw_url = "https://www.kabum.com.br"
+    raw_url: str = field(default="https://www.kabum.com.br", init=False)
+    query_string: str = field(
+        default="?page_number={page_number}&page_size=100&facet_filters=&sort=most_searched",
+        init=False,
+    )
 
-    def get_volatile_data(self, URL: str) -> Tuple[URL, str, Money, int]:
-        html = rq.get(URL).content
+    def get_volatile_data(
+        self, url: str
+    ) -> tuple[URL | None, list[tuple[URL, str, Money, int]]]:
+        headers = {
+            "User-Agent": "Mozilla/5.0",
+        }
+        html = rq.get(url, headers=headers).content
         soup = BeautifulSoup(html, "html.parser")
-        availability = [
-            if element.string == "COMPRAR"
-            for element in soup.select(
-                "div.sc-6061e719-4.sc-6061e719-6.flAjw.gvThCo.availableFooterCard button"
-            )
-        ]
 
         prices = [
-            Money(float(element.string))
-            for element in soup.select("span.sc-3b515ca1-2.eqqhbT.priceCard")
+            Money(
+                float(
+                    element.string.split("\u00A0")[-1]
+                    .replace(".", "")
+                    .replace(",", ".")
+                )
+            )
+            if not element.string.split("\u00A0")[-1] == "---"
+            else Money(-1)
+            for element in soup.select("span.sc-3b515ca1-2")
+        ]
+
+        availability = [
+            False if element.string.split("\u00A0")[-1] == "---" else True
+            for element in soup.select("span.sc-3b515ca1-2")
         ]
 
         names = [
@@ -31,11 +48,26 @@ class KabumScraper(AbstractScraper):
             )
         ]
 
-        links = [element["href"] for element in soup.select("a.sc-ff8a9791-10.htpbqG")]
-        
+        links = [
+            URL.get_URL(str(self.raw_url + element["href"]))
+            for element in soup.select("a.sc-ff8a9791-10.htpbqG")
+        ]
+
         volatile_data = []
         for link, name, price, availability in zip(links, names, prices, availability):
-            volatile_data.append(tuple(link, name, price, availability))
+            volatile_data.append(tuple([link, name, price, availability]))
 
+        number_of_pages = [int(element.string) for element in soup.select("a.page")]
 
-        return volatile_data
+        n_actual_page = int(soup.select_one("a.page.active").string)
+
+        n_next_page = n_actual_page + 1
+
+        if n_next_page in number_of_pages:
+            next_page = url.split("?")[0] + self.query_string.format(
+                page_number=n_next_page
+            )
+        else:
+            next_page = None
+
+        return next_page, volatile_data
