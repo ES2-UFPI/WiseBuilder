39edf6945806989c4f1da7eba1b05f94c41f8e5a
Lucas Bandeira <Lucasbanmir@hotmail.com>
2023-03-01 23:18:11 -0300

    Criando menu de navegação do site #23

--- src/entrypoints/web/wise-builder/src/index.tsx
+++ src/entrypoints/web/wise-builder/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './pages/CadastroComp';
+import MenuNav from './components/MenuNav/menuNav';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <MenuNav />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
