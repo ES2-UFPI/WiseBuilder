8ba341f8835079157e6c788245e7189f8be063fb
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-03-06 19:32:42 -0300

    colocando tipos nas variaveis #29

--- src/Scraper/domain/scrapers.py
+++ src/Scraper/domain/scrapers.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from Scraper.domain.value_object import AbstractScraper
 from framework.domain.value_object import URL, Money
 from bs4 import BeautifulSoup
-import requests as rq
+from requests import get
 
 
 @dataclass(init=False)
@@ -14,15 +14,28 @@ class KabumScraper(AbstractScraper):
 
     def get_volatile_data(
         self, url: str
-    ) -> tuple[URL | None, list[tuple[URL, str, Money, int]]]:
+    ) -> tuple[URL | None, list[tuple[URL, str, Money, bool]]]:
         headers = {
             "User-Agent": "Mozilla/5.0",
         }
 
-        html = rq.get(url, headers=headers).content
+        html = get(url, headers=headers).content
+
         soup = BeautifulSoup(html, "html.parser")
 
-        prices = [
+        links: list[URL] = [
+            URL.get_URL(str(self.raw_url + element["href"]))
+            for element in soup.select("a.sc-ff8a9791-10.htpbqG")
+        ]
+
+        names: list[str] = [
+            element.string
+            for element in soup.select(
+                "span.sc-d99ca57-0.cpPIRA.sc-ff8a9791-16.dubjqF.nameCard"
+            )
+        ]
+
+        prices: list[Money] = [
             Money(
                 float(
                     element.string.split("\u00A0")[-1]
@@ -35,32 +48,27 @@ class KabumScraper(AbstractScraper):
             for element in soup.select("span.sc-3b515ca1-2")
         ]
 
-        availability = [
+        availability: list[bool] = [
             False if element.string.split("\u00A0")[-1] == "---" else True
             for element in soup.select("span.sc-3b515ca1-2")
         ]
 
-        names = [
-            element.string
-            for element in soup.select(
-                "span.sc-d99ca57-0.cpPIRA.sc-ff8a9791-16.dubjqF.nameCard"
-            )
-        ]
-
-        links = [
-            URL.get_URL(str(self.raw_url + element["href"]))
-            for element in soup.select("a.sc-ff8a9791-10.htpbqG")
-        ]
-
-        volatile_data = []
-        for link, name, price, availability in zip(links, names, prices, availability):
+        volatile_data: list[tuple[URL, str, Money, int]] = []
+        for link, name, price, availability in zip(
+            links,
+            names,
+            prices,
+            availability,
+        ):
             volatile_data.append(tuple([link, name, price, availability]))
 
-        number_of_pages = [int(element.string) for element in soup.select("a.page")]
+        number_of_pages: int = [
+            int(element.string) for element in soup.select("a.page")
+        ]
 
-        n_actual_page = int(soup.select_one("a.page.active").string)
+        n_actual_page: int = int(soup.select_one("a.page.active").string)
 
-        n_next_page = n_actual_page + 1
+        n_next_page: int = n_actual_page + 1
 
         if n_next_page in number_of_pages:
             next_page = url.split("?")[0] + self.query_string.format(
