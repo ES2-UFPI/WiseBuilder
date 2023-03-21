c415d597e9718d0536e539ba1b6f668cda770f89
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-03-21 09:23:15 -0300

    criacao de endpoint #31

--- src/entrypoints/api/endpoints/api.py
+++ src/entrypoints/api/endpoints/api.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
 from flask_restx import Api
-from .cpus import cpu_namespace
-from .gpus import gpu_namespace
-from .psus import psu_namespace
-from .motherboards import motherboard_namespace
-from .persistence import persistence_namespace
-from .ram import ram_namespace
+# from .cpus import cpu_namespace
+# from .gpus import gpu_namespace
+# from .psus import psu_namespace
+# from .motherboards import motherboard_namespace
+# from .persistence import persistence_namespace
+# from .ram import ram_namespace
+from .search import search_namespace
 
 blueprint: Blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api: Api = Api(
@@ -15,9 +16,10 @@ api: Api = Api(
     description="API para consulta e montagem de componentes",
 )
 
-api.add_namespace(cpu_namespace, "/cpus")
-api.add_namespace(gpu_namespace, "/gpus")
-api.add_namespace(psu_namespace, "/psus")
-api.add_namespace(motherboard_namespace, "/motherboards")
-api.add_namespace(persistence_namespace, "/persistences")
-api.add_namespace(ram_namespace, "/rams")
+# api.add_namespace(cpu_namespace, "/cpus")
+# api.add_namespace(gpu_namespace, "/gpus")
+# api.add_namespace(psu_namespace, "/psus")
+# api.add_namespace(motherboard_namespace, "/motherboards")
+# api.add_namespace(persistence_namespace, "/persistences")
+# api.add_namespace(ram_namespace, "/rams")
+api.add_namespace(search_namespace, "/search")
---
+++ src/entrypoints/api/endpoints/search.py
@@ -0,0 +1,22 @@
+from flask_restx import Namespace, Resource, reqparse
+from dataclasses import dataclass
+
+search_namespace = Namespace("Search")
+
+parser = reqparse.RequestParser()
+parser.add_argument("find", type=str, help="", required=True)
+parser.add_argument("component_type", type=str)
+
+
+@dataclass
+class AbstractSearchEngine:
+    @classmethod
+    def search(cls, **kwargs):
+        return {"type": "gpu", "model": "gtx 1080"}
+
+
+@search_namespace.route("")
+class SearchList(Resource):
+    @search_namespace.doc(parser=parser)
+    def get(self):
+        return AbstractSearchEngine.search(**parser.parse_args())
