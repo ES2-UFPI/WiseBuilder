70430d846fcf6062bc0a1a236079f2d2b7bd5475
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-03-23 09:29:55 -0300

    criacao da inicial da mochila #52

--- src/framework/domain/exception.py
+++ src/framework/domain/exception.py
@@ -9,3 +9,13 @@ class DomainException(Exception):
 
     def __repr__(self):
         return f"{self.__class__.__name__}: {self._message}"
+
+
+@dataclass
+class KnapsackBurst(DomainException):
+    _message: str = "A bolsa atingiu o limite de preço."
+
+
+@dataclass
+class CurrencyNotEqual(DomainException):
+    _message: str = "As moedas são diferentes"
