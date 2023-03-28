978358947c7187f8f8320e22a82cb65597a8cd7c
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-03-28 05:48:02 -0300

    adicao de parametros da motherboard #32

--- src/entrypoints/web/wise-builder/src/types/componente.ts
+++ src/entrypoints/web/wise-builder/src/types/componente.ts
@@ -4,9 +4,9 @@ export interface Componente {
   manufacturer: string;
   price?: number;
   link?: string;
-  socket?: number;
+  socket?: string;
   n_cores?: number;
-  chipset?: number;
+  chipset?: string;
   board_size?: number;
   n_ram_slots?: number;
   n_usb2?: number;
